98ce97b67506710af9ded67d82a84418e8381f8d
yili <1270993539@qq.com>
2020-06-01 22:02:23 +0800

    add curd brand liyili

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -17,60 +17,62 @@ import entity.Result;
 @RequestMapping("/brand")
 public class BrandController {
 
-	@Reference
-	private BrandService bService;
-	@RequestMapping("/findAll")
-	public List<TbBrand> findAll(){
-		
-		return bService.findAll();
-	}
+    @Reference
+    private BrandService bService;
 
-	@RequestMapping("/findPage")
-	public PageResult findPageResult(int page,int size) {
-		 
-		return bService.findPage(page, size);
-	}
+    @RequestMapping("/findAll")
+    public List<TbBrand> findAll() {
 
-	@RequestMapping("/add")
-	public Result add(@RequestBody TbBrand tbBrand) {
-		try {
-			bService.add(tbBrand);
-		return new Result("新增品牌成功", true);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result("新增品牌失败", false);
-		}
-	}
-	@RequestMapping("/findOne")
-	public TbBrand findOne(Long id) {
-		return bService.findOne(id);
-	}
-	@RequestMapping("/update")
-	public Result update(@RequestBody TbBrand tbBrand) {
-		try {
-			bService.update(tbBrand);
-		return new Result("修改品牌成功", true);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result("修改品牌失败", false);
-		}
-	}
-	@RequestMapping("/delete")
-	public Result delete(Long[] longs) {
-		try {
-			bService.delete(longs);
-		return new Result("删除品牌成功", true);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result("删除品牌失败", false);
-		}
-	}
-	@RequestMapping("/search")
-	public PageResult search(@RequestBody TbBrand tbBrand,int page,int size) {
-		return bService.findPage(tbBrand, page, size);
-	}
-	
-	
-	
+        return bService.findAll();
+    }
+
+    @RequestMapping("/findPage")
+    public PageResult findPageResult(int page, int size) {
+
+        return bService.findPage(page, size);
+    }
+
+    @RequestMapping("/add")
+    public Result add(@RequestBody TbBrand tbBrand) {
+        try {
+            bService.add(tbBrand);
+            return new Result("新增品牌成功", true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result("新增品牌失败", false);
+        }
+    }
+
+    @RequestMapping("/findOne")
+    public TbBrand findOne(Long id) {
+        return bService.findOne(id);
+    }
+
+    @RequestMapping("/update")
+    public Result update(@RequestBody TbBrand tbBrand) {
+        try {
+            bService.update(tbBrand);
+            return new Result("修改品牌成功", true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result("修改品牌失败", false);
+        }
+    }
+
+    @RequestMapping("/delete")
+    public Result delete(Long[] longs) {
+        try {
+            bService.delete(longs);
+            return new Result("删除品牌成功", true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result("删除品牌失败", false);
+        }
+    }
+
+    @RequestMapping("/search")
+    public PageResult search(@RequestBody TbBrand tbBrand, int page, int size) {
+        return bService.findPage(tbBrand, page, size);
+    }
+
 }
-
--- pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -15,71 +15,70 @@ import com.pinyougou.sellergoods.service.BrandService;
 
 import entity.PageResult;
 import util.LylValidateUtils;
+
 @Service
 public class BrandServiceImpl implements BrandService {
 
-	@Autowired
-	private TbBrandMapper brandMapper;
-	
-	@Override
-	public List<TbBrand> findAll() {
-		
-		return brandMapper.selectByExample(null);
-	}
+    @Autowired
+    private TbBrandMapper brandMapper;
 
-	@Override
-	public PageResult findPage(int pageNum, int rows) {
-		
-		PageHelper.startPage(pageNum, rows);//分页
-		
-		Page<TbBrand>  page=(Page<TbBrand>) brandMapper.selectByExample(null);
-		
-		return new PageResult(page.getTotal(), page.getResult());
-	}
+    @Override
+    public List<TbBrand> findAll() {
 
-	@Override
-	public void add(TbBrand tbBrand) {
-		brandMapper.insert(tbBrand);
-	}
+        return brandMapper.selectByExample(null);
+    }
 
-	@Override
-	public TbBrand findOne(Long id) {
-		return brandMapper.selectByPrimaryKey(id);
-	}
+    @Override
+    public PageResult findPage(int pageNum, int rows) {
 
-	@Override
-	public void update(TbBrand tbBrand) {
-		brandMapper.updateByPrimaryKey(tbBrand);
-	}
+        PageHelper.startPage(pageNum, rows);// 分页
 
-	@Override
-	public void delete(Long[] ids) {
-		for(Long id:ids) {
-			brandMapper.deleteByPrimaryKey(id);
-		}
-		
-	}
+        Page<TbBrand> page = (Page<TbBrand>)brandMapper.selectByExample(null);
 
-	@Override
-	public PageResult findPage(TbBrand tbBrand, int pageNum, int rows) {
-		
-		PageHelper.startPage(pageNum, rows);//分页
-		TbBrandExample tbBrandExample=new TbBrandExample();
-		Criteria criteria = tbBrandExample.createCriteria();
-		if (tbBrand!=null) {
-			if (!LylValidateUtils.isEmpty(tbBrand.getName())) {
-				criteria.andNameLike("%"+tbBrand.getName()+"%");
-			}
-			if (!LylValidateUtils.isEmpty(tbBrand.getFirstChar())) {
-				criteria.andFirstCharLike("%"+tbBrand.getFirstChar()+"%");
-			}
-		}
-		
-		Page<TbBrand>  page=(Page<TbBrand>) brandMapper.selectByExample(tbBrandExample);
-		
-		return new PageResult(page.getTotal(),page.getResult());
-	}
-	
-	
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+    @Override
+    public void add(TbBrand tbBrand) {
+        brandMapper.insert(tbBrand);
+    }
+
+    @Override
+    public TbBrand findOne(Long id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(TbBrand tbBrand) {
+        brandMapper.updateByPrimaryKey(tbBrand);
+    }
+
+    @Override
+    public void delete(Long[] ids) {
+        for (Long id : ids) {
+            brandMapper.deleteByPrimaryKey(id);
+        }
+
+    }
+
+    @Override
+    public PageResult findPage(TbBrand tbBrand, int pageNum, int rows) {
+
+        PageHelper.startPage(pageNum, rows);// 分页
+        TbBrandExample tbBrandExample = new TbBrandExample();
+        Criteria criteria = tbBrandExample.createCriteria();
+        if (tbBrand != null) {
+            if (!LylValidateUtils.isEmpty(tbBrand.getName())) {
+                criteria.andNameLike("%" + tbBrand.getName() + "%");
+            }
+            if (!LylValidateUtils.isEmpty(tbBrand.getFirstChar())) {
+                criteria.andFirstCharLike("%" + tbBrand.getFirstChar() + "%");
+            }
+        }
+
+        Page<TbBrand> page = (Page<TbBrand>)brandMapper.selectByExample(tbBrandExample);
+
+        return new PageResult(page.getTotal(), page.getResult());
+    }
 
 }
